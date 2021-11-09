0d7617974cddbe3f77b6a29a0d5f5b4f9cf2b1c6
Maryna-Bazylevska <83463971+Maryna-Bazylevska@users.noreply.github.com>
2021-11-09 12:48:46 +0100

    add

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
 import './sass/main.scss';
+import refs from './js/refs';
+import PixabayApiServise from './js/apiService';
+import ImageItemTemplate from './templates/ImageItemTemplate';
+import '@pnotify/core/dist/BrightTheme.css';
+import { info, error } from '@pnotify/core';
+refs.searchForm.addEventListener('submit', onSearch);
+refs.loadMore.addEventListener('click', onLoadMore);
+const apiServise = new PixabayApiServise();
+function onSearch(e){
+  e.preventDefault();
+  apiServise.query = e.currentTarget.elements.query.value;
+  if (apiServise.query === '') {
+    return error({
+      text: 'Please, enter a text!',
+      delay: 500,
+    });
+  }
+  apiServise.resetPage();
+  clearListItems();
+  onLoadMore();
+}
+function onLoadMore(e) {
+  apiServise
+    .fetchImages()
+    .then(data => onMarkUp(data))
+    .catch(data => alert(`Ошибка: ${data}`));
+}
+
+function onMarkUp(data) {
+  refs.loadMore.style.display = 'block';
+  refs.gallery.insertAdjacentHTML('beforeend', ImageItemTemplate(data));
+  
+}
+
+
+function clearListItems(){
+  refs.gallery.innerHTML = '';
+}
